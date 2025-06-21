d4374981043ca47bd5d8150b45297b76d6915bcb
Feri Romadhona <feri.romadhona@mncgroup.com>
2025-06-22 01:45:19 +0700

    prepare for github pages deploy

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   transpilePackages: ["framer-motion"],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
