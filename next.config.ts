366c5b2eb94da70ee822ba4fb560891c0e0c4337
Feri Romadhona <feri.romadhona@mncgroup.com>
2025-06-23 00:56:06 +0700

    prepare for github pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   transpilePackages: ["framer-motion"],
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // basePath: "/FERI-PORTOFOLIO",
-  // assetPrefix: "/FERI-PORTOFOLIO/",
   env: {
     NEXT_PUBLIC_BASE_PATH: '',
   },
